e334c419ff75338d8a6da8d28dada07276188076
Lizaveta <143902215+lizaveta-yakimenka@users.noreply.github.com>
2024-11-06 14:53:36 -0600

    Update lyakimenka_test.py
    Remove semi-colon

--- lyakimenka_test.py
+++ lyakimenka_test.py
@@ -10,11 +10,11 @@ options.add_argument('--no-sandbox')
 options.add_argument('--headless')
 options.add_argument('--disable-dev-shm-usage')
 
-options.add_argument("start-maximized"); 
-options.add_argument("disable-infobars"); 
-options.add_argument("--disable-extensions"); 
-options.add_argument("--disable-gpu"); 
-options.add_argument("--disable-dev-shm-usage"); 
+options.add_argument("start-maximized") 
+options.add_argument("disable-infobars") 
+options.add_argument("--disable-extensions") 
+options.add_argument("--disable-gpu") 
+options.add_argument("--disable-dev-shm-usage") 
 try:
   driver = webdriver.Chrome(options=options)
 
